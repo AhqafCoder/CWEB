99a0ca57ec23d4478e913983511cd5cb50cb2cf4
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-02-04 17:01:45 +0000

    deleted search.js and added it funtion in the page with other component

--- www/search.js
+++
@@ -1,9 +0,0 @@
-
-function searchStudents() {
-    const input = document.getElementById('search-input').value.toLowerCase();
-    const rows = document.querySelectorAll('#student-table tbody tr');
-    rows.forEach(row => {
-        const studentData = row.textContent.toLowerCase();
-        row.style.display = studentData.includes(input) ? '' : 'none';
-    });
-}
